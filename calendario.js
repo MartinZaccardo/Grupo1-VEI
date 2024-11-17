04c14aad53ebf6211d3827702db03d949dc7ee74
ValenMelito <143010111+ValenMelito@users.noreply.github.com>
2024-11-17 19:29:11 -0300

    elimino archivos los movi a todos en capetas para mas orden y cambie los links

--- calendario.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const calendarInput = document.getElementById("calendar");
-    const today = new Date().toISOString().split("T")[0];
-    calendarInput.setAttribute("min", today);
-});
--- turnos.js
+++
@@ -1,5 +0,0 @@
-    document.querySelector('form').addEventListener('submit', function() {
-        // Almacenar valores de especialidad y médico en localStorage
-        localStorage.setItem('especialidad', document.getElementById('especialidad').value);
-        localStorage.setItem('medico', document.getElementById('medico').value);
-    });
